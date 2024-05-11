c7160b0c382cc5c559f9d637bd349152604320d7
Alexanua <86alesha@gafe.molndal.se>
2024-05-11 03:40:43 +0200

    andra

--- src/test/java/warehouse/AlexWarehouse/AlexWarehouseApplicationTests.java
+++ src/test/java/warehouse/AlexWarehouse/AlexWarehouseApplicationTests.java
@@ -2,11 +2,13 @@ package warehouse.AlexWarehouse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AlexWarehouseApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
 }
+
